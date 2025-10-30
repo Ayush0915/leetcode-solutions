65879e744d07775aada7b85da96a4a0e007d4540
Ayush Kumar Bhadani <145894529+Ayush0915@users.noreply.github.com>
2025-10-30 05:36:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.6 MB (43.33%)

---
+++ 0035-search-insert-position/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int high = n - 1;
+        int low = 0, ans = n;
+        while (high >= low) {
+            int mid = (high + low) / 2;
+
+            if (nums[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+
+            } else
+                low = mid + 1;
+        }
+        return ans;
+    }
+};
